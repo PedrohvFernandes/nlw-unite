c16b6a9b94ef8f1cc90fe25e0457133a575256c0
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2024-04-07 13:11:44 -0300

    feat: add count attendees

--- api/src/routes/get-event-attendees.ts
+++ api/src/routes/get-event-attendees.ts
@@ -43,9 +43,10 @@ export async function getEventAttendees(app: FastifyInstance) {
                 //     value ? new Date(value).toISOString() : null
                 //   )
                 // checkIn: z.date().nullish() // z.date().optional()
-                checkIn: z.date().nullable()
+                checkedInAt: z.date().nullable()
               })
-            )
+            ),
+            totalAttendees: z.number()
           })
         }
       }
@@ -54,50 +55,65 @@ export async function getEventAttendees(app: FastifyInstance) {
       const { eventId } = request.params
       const { pageIndex, query } = request.query
 
-      // findMany retorna varios
-      const attendees = await prisma.attendee.findMany({
-        select: {
-          id: true,
-          name: true,
-          email: true,
-          createdAt: true,
-          // Quando o usuario realizou o check-in
-          checkIn: {
-            select: {
-              createdAt: true
+      const [attendees, totalAttendees] = await Promise.all([
+        // findMany retorna varios
+        prisma.attendee.findMany({
+          select: {
+            id: true,
+            name: true,
+            email: true,
+            createdAt: true,
+            // Quando o usuario realizou o check-in
+            checkIn: {
+              select: {
+                createdAt: true
+              }
             }
+          },
+          // Se eu tiver a busca eu busco tanto pelo id quanto pelo nome ou email, se eu não tiver a busca eu busco apenas pelo id
+          where: query
+            ? {
+                // Sistema de busca
+                eventId,
+                OR: [
+                  {
+                    name: {
+                      contains: query
+                    }
+                  },
+                  {
+                    email: {
+                      contains: query
+                    }
+                  }
+                ]
+              }
+            : {
+                eventId
+              },
+          // Criando sistema de paginação, para retornar de 10 registro por pargina
+          take: 10,
+          // Quantos registros eu vou pular, de quanto em quanto(10 em 10 por exemplo), se eu tenho uma lista de 20 registros(participantes) e quero retornar os ultimos 10, eu vou pular os 10 primeiros. Ex: 20/10 = 2, 2 * 10 = 20, então eu pulei os 10 primeiros registros. Logo o pageIndex precisa ser 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12... para eu conseguir pegar todos os registros. Então o pageIndex multiplica por 10 para pular os registros, se ele for 3 pegamos os registros de 30 a 40. 0 * 10 = 0, logo não pulamos nenhum registro, 1 * 10 = 10, logo pulamos 10 registros, 2 * 10 = 20, logo pulamos 20 registros...
+          skip: pageIndex * 10,
+          orderBy: {
+            // Ordenando por data de criação, do mais recente para o mais antigo
+            createdAt: 'desc'
           }
-        },
-        // Se eu tiver a busca eu busco tanto pelo id quanto pelo nome ou email, se eu não tiver a busca eu busco apenas pelo id
-        where: query
-          ? {
-              // Sistema de busca
-              eventId,
-              OR: [
-                {
-                  name: {
-                    contains: query
-                  }
-                },
-                {
-                  email: {
-                    contains: query
-                  }
+        }),
+        prisma.attendee.count({
+          // Se tiver a query eu busco o total pelo nome e o id do evento, se não eu busco o total somente pelo id do evento
+          where: query
+            ? {
+                eventId,
+                name: {
+                  contains: query
                 }
-              ]
-            }
-          : {
-              eventId
-            },
-        // Criando sistema de paginação, para retornar de 10 registro por pargina
-        take: 10,
-        // Quantos registros eu vou pular, de quanto em quanto(10 em 10 por exemplo), se eu tenho uma lista de 20 registros(participantes) e quero retornar os ultimos 10, eu vou pular os 10 primeiros. Ex: 20/10 = 2, 2 * 10 = 20, então eu pulei os 10 primeiros registros. Logo o pageIndex precisa ser 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12... para eu conseguir pegar todos os registros. Então o pageIndex multiplica por 10 para pular os registros, se ele for 3 pegamos os registros de 30 a 40. 0 * 10 = 0, logo não pulamos nenhum registro, 1 * 10 = 10, logo pulamos 10 registros, 2 * 10 = 20, logo pulamos 20 registros...
-        skip: pageIndex * 10,
-        orderBy: {
-          // Ordenando por data de criação, do mais recente para o mais antigo
-          createdAt: 'desc'
-        }
-      })
+              }
+            : {
+                eventId
+              }
+        })
+      ])
 
       return reply.send({
         attendees: attendees.map((attendee) => ({
@@ -106,8 +122,9 @@ export async function getEventAttendees(app: FastifyInstance) {
           email: attendee.email,
           createdAt: attendee.createdAt,
           // checkIn: attendee.checkIn?.createdAt // attendee.checkIn ? attendee.checkIn.createdAt : null
-          checkIn: attendee.checkIn?.createdAt ?? null
-        }))
+          checkedInAt: attendee.checkIn?.createdAt ?? null
+        })),
+        totalAttendees
       })
     }
   )

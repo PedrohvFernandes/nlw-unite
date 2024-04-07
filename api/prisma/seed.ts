0603bf5f1a62531a6a3badd8a0dd7999db805ee3
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2024-04-07 12:22:56 -0300

    docs: comment

--- api/prisma/seed.ts
+++ api/prisma/seed.ts
@@ -6,7 +6,7 @@ import dayjs from 'dayjs'
 
 async function seed() {
   // https://www.uuidgenerator.net
-  const eventId = faker.string.uuid() // ex: '9e9bd979-9d10-4915-b339-3786b1634f33'
+  const eventId = faker.string.uuid() // ex: '9e9bd979-9d10-4915-b339-3786b1634f33' pode deixa fixo caso deseje
 
   await prisma.event.deleteMany()
 
